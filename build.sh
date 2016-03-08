5ce06875a32ffaf450cb731f85124f63cad6f493
Josh Baker <joshbaker77@gmail.com>
2016-03-08 06:11:03 -0700

    allow optional protected-mode

--- build.sh
+++ build.sh
@@ -2,9 +2,7 @@
 set -e
 
 VERSION="0.0.1"
-BUILD_TIME=$(date +%FT%T%z)
-GIT_SHA=$(git rev-parse --short HEAD)
-LDFLAGS="-X github.com/tidwall/geoengine/core.Version=${VERSION} -X github.com/tidwall/geoengine/core.BuildTime=${BUILD_TIME} -X github.com/tidwall/geoengine/core.GitSHA=${GIT_SHA}"
+PROTECTED_MODE="no" 
 
 export GO15VENDOREXPERIMENT=1
 
@@ -33,6 +31,13 @@ if [ "$NOCOPY" != "1" ]; then
 fi
 
 core/gen.sh
+
+LDFLAGS="$LDFLAGS -X github.com/tidwall/geoengine/core.Version=${VERSION}"
+LDFLAGS="$LDFLAGS -X github.com/tidwall/geoengine/core.GitSHA=$(git rev-parse --short HEAD)"
+LDFLAGS="$LDFLAGS -X github.com/tidwall/geoengine/core.BuildTime=$(date +%FT%T%z)"
+if [ "$PROTECTED_MODE" == "no" ]; then
+	LDFLAGS="$LDFLAGS -X github.com/tidwall/geoengine/core.ProtectedMode=no"
+fi
 
 # build and store objects into original directory.
 go build -ldflags "$LDFLAGS" -o "$OD/geoengine-server" cmd/geoengine-server/*.go
