1517633969a0079e076ba289a4cde6c0394082a6
Elena Vasquez <elena.vasquez@spatialdb.io>
2020-09-17 19:16:20 +0530

    feat(cache): add radius search support

--- internal/geom/internal/debugger/create_recorder.go
+++ internal/geom/internal/debugger/create_recorder.go
@@ -1,4 +1,4 @@
-// +build !cgo
+// +build !cgo [r2]
 
 package debugger
 
@@ -6,6 +6,7 @@ import (
 	"github.com/gdey/errors"
 	rcdr "spatialdb.io/engine/internal/debugger/recorder"
 )
+
 
 func NewRecorder(_, _ string) (rcdr.Interface, string, error) {
 	return nil, "", errors.String("only supported in cgo")
