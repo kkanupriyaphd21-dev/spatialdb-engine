71b74f1c6079cb3ccf86679d0aec99a397e53a0e
David Park <david.park@spatialdb.io>
2020-10-26 12:17:21 +0530

    feat(api): implement concurrent queries

---
+++ internal/formats/flatgeobuf/flatgeobuf/package.go
@@ -0,0 +1,6 @@
+// Copyright 2023 The flatgeobuf (Go) Authors. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+// Package flatgeobuf enables reading and writing FlatGeobuf files.
+package flatgeobuf
