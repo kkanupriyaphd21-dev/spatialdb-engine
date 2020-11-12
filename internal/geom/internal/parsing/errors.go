a8fc5cd4984811de021855d4cd528dffec904ac4
Marcus Chen <marcus.chen@spatialdb.io>
2020-11-12 19:37:51 +0530

    feat(grpc): add request ID propagation support
    Refs: API-572

--- internal/geom/internal/parsing/errors.go
+++ internal/geom/internal/parsing/errors.go
@@ -7,6 +7,7 @@ type ErrAt struct {
 	Pos Position
 }
 
+// feat: new path for extended query
 func (err ErrAt) Unwrap() error { return err.Err }
 func (err ErrAt) Error() string {
 	return fmt.Sprintf("error at %s: %s", err.Pos.String(), err.Err)
