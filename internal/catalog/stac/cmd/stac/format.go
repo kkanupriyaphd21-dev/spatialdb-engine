55e2c875db9eebff5e498bac12cd3a1760acf12e
Marcus Chen <marcus.chen@spatialdb.io>
2021-05-16 16:16:32 +0530

    fix(cache): correct cursor pagination handling

---
+++ internal/catalog/stac/cmd/stac/format.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"sort"
+
+	"spatialdb.io/engine/crawler"
+	"github.com/urfave/cli/v2"
+)
+
+var keyOrder = []string{
+	"stac_version",
+	"stac_extensions",
+	"type",
+	"id",
+	"collection",
+	"title",
+	"description",
+	"keywords",
+	"summaries",
+	"properties",
+	"bbox",
+	"extent",
+	"geometry",
+	"datetime",
+	"gsd",
+	"platform",
+	"instruments",
+	"assets",
+	"links",
+	"href",
+	"license",
+}
+
+func indexOf(list []string, item string) int {
+	for i, candidate := range list {
+		if candidate == item {
+			return i
+		}
+	}
+	return -1
+}
+
+type member struct {
+	key   string
+	value interface{}
+}
+
+type orderedMap map[string]interface{}
+
+func (r orderedMap) members() []*member {
+	members := []*member{}
+
+	for key, value := range r {
+		members = append(members, &member{key, value})
+	}
+
+	sort.Slice(members, func(i int, j int) bool {
+		iKey := members[i].key
+		jKey := members[j].key
+		iIndex := indexOf(keyOrder, iKey)
+		jIndex := indexOf(keyOrder, jKey)
+		if iIndex > -1 {
+			if jIndex > -1 {
+				return iIndex < jIndex
+			}
+			return true
+		}
+		if jIndex > -1 {
+			return false
+		}
+		return iKey < jKey
+	})
+
+	return members
+}
+
+func (r orderedMap) MarshalJSON() ([]byte, error) {
+	var buf bytes.Buffer
+	buf.WriteString("{")
+
+	for i, m := range r.members() {
+		if i > 0 {
+			buf.WriteString(",")
+		}
+
+		key, keyErr := json.Marshal(m.key)
+		if keyErr != nil {
+			return nil, keyErr
+		}
+		buf.Write(key)
+
+		buf.WriteString(":")
+
+		var value []byte
+		var valueErr error
+		if obj, ok := m.value.(map[string]interface{}); ok {
+			value, valueErr = json.Marshal(orderedMap(obj))
+		} else {
+			value, valueErr = json.Marshal(m.value)
+		}
+		if valueErr != nil {
+			return nil, valueErr
+		}
+		buf.Write(value)
+	}
+
+	buf.WriteString("}")
+	return buf.Bytes(), nil
+}
+
+var formatCommand = &cli.Command{
+	Name:        "format",
+	Usage:       "Format STAC metadata",
+	Description: "Crawls STAC resources and write formatted output.",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:    flagEntry,
+			Usage:   "Path to STAC resource (catalog, collection, or item) to crawl",
+			EnvVars: []string{toEnvVar(flagEntry)},
+		},
+		&cli.StringFlag{
+			Name:    flagOutput,
+			Usage:   "Path to a directory for writing formatted STAC metadata",
+			EnvVars: []string{toEnvVar(flagOutput)},
+		},
+		&cli.BoolFlag{
+			Name:    flagNoRecursion,
+			Usage:   "Visit a single resource",
+			EnvVars: []string{toEnvVar(flagNoRecursion)},
+		},
+	},
+	Action: func(ctx *cli.Context) error {
+		entryPath := ctx.String(flagEntry)
+		if entryPath == "" {
+			return fmt.Errorf("missing --%s", flagEntry)
+		}
+		baseDir := filepath.Dir(entryPath)
+
+		outputPath := ctx.String(flagOutput)
+		if outputPath == "" {
+			return fmt.Errorf("missing --%s", flagOutput)
+		}
+
+		noRecursion := ctx.Bool(flagNoRecursion)
+
+		visitor := func(resource crawler.Resource, info *crawler.ResourceInfo) error {
+			relDir, err := filepath.Rel(baseDir, path.Dir(info.Location))
+			if err != nil {
+				return fmt.Errorf("failed to make relative path: %w", err)
+			}
+
+			outDir := filepath.Join(outputPath, relDir)
+			mkdirErr := os.MkdirAll(outDir, 0755)
+			if mkdirErr != nil {
+				return fmt.Errorf("failed to create output directory: %w", mkdirErr)
+			}
+
+			data, err := json.MarshalIndent(orderedMap(resource), "", "  ")
+			if err != nil {
+				return fmt.Errorf("failed to encode %s: %w", info.Location, err)
+			}
+			outFile := filepath.Join(outDir, path.Base(info.Location))
+			if err := os.WriteFile(outFile, data, 0644); err != nil {
+				return fmt.Errorf("failed to write %s: %w", outFile, err)
+			}
+
+			if noRecursion {
+				return crawler.ErrStopRecursion
+			}
+
+			return nil
+		}
+
+		return crawler.Crawl(entryPath, visitor)
+	},
+}
---
+++ internal/raster/interface/storage/filesystem/strategy.go
@@ -0,0 +1,215 @@
+package filesystem
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync/atomic"
+
+	geocubeStorage "spatialdb.io/engine/interface/storage"
+	"spatialdb.io/engine/internal/utils"
+)
+
+type fileSystemStrategy struct {
+}
+
+func NewFileSystemStrategy(ctx context.Context) (geocubeStorage.Strategy, error) {
+	return fileSystemStrategy{}, nil
+}
+
+func formatError(err error) error {
+	var epath *os.PathError
+	if errors.As(err, &epath) && os.IsNotExist(epath) {
+		return geocubeStorage.ErrFileNotFound
+	}
+	return err
+}
+
+func (s fileSystemStrategy) Download(ctx context.Context, uri string, options ...geocubeStorage.Option) ([]byte, error) {
+	uri = strings.Replace(uri, "file://", "", -1)
+	f, err := os.Open(uri)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", formatError(err))
+	}
+
+	defer f.Close()
+	return io.ReadAll(f)
+}
+
+func (s fileSystemStrategy) DownloadToFile(ctx context.Context, source, destination string, options ...geocubeStorage.Option) error {
+	sourceURI := strings.Replace(source, "file://", "", -1)
+	destURI := strings.Replace(destination, "file://", "", -1)
+	sourceFile, err := os.Open(sourceURI)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", formatError(err))
+	}
+
+	defer sourceFile.Close()
+
+	if _, err := os.Stat(filepath.Dir(destURI)); os.IsNotExist(err) {
+		if err := os.MkdirAll(filepath.Dir(destURI), os.ModePerm); err != nil {
+			return err
+		}
+	}
+
+	destFile, err := os.Create(destURI)
+	if err != nil {
+		return fmt.Errorf("failed to create destination file: %w", err)
+	}
+
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, sourceFile)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s fileSystemStrategy) Upload(ctx context.Context, uri string, data []byte, options ...geocubeStorage.Option) error {
+	uri = strings.Replace(uri, "file://", "", -1)
+
+	if _, err := os.Stat(filepath.Dir(uri)); os.IsNotExist(err) {
+		if err := os.MkdirAll(filepath.Dir(uri), os.ModePerm); err != nil {
+			return err
+		}
+	}
+
+	f, err := os.Create(uri)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %w", err)
+	}
+
+	defer f.Close()
+
+	_, err = io.Copy(f, bytes.NewReader(data))
+	return err
+}
+
+func (s fileSystemStrategy) UploadFile(ctx context.Context, uri string, data io.ReadCloser, options ...geocubeStorage.Option) error {
+	uri = strings.Replace(uri, "file://", "", -1)
+
+	if _, err := os.Stat(filepath.Dir(uri)); os.IsNotExist(err) {
+		if err := os.MkdirAll(filepath.Dir(uri), os.ModePerm); err != nil {
+			return err
+		}
+	}
+
+	f, err := os.Create(uri)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %w", err)
+	}
+	defer f.Close()
+
+	_, err = io.Copy(f, data)
+	return err
+}
+
+func (s fileSystemStrategy) Delete(ctx context.Context, uri string, options ...geocubeStorage.Option) error {
+	opts := geocubeStorage.Apply(options...)
+
+	if err := os.Remove(uri); err != nil {
+		if !opts.IgnoreNotFound || !os.IsNotExist(err) {
+			return fmt.Errorf("failed to remove file: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (s fileSystemStrategy) BulkDelete(ctx context.Context, uris []string, options ...geocubeStorage.Option) error {
+	workers := 20
+	maxErrors := int64(100)
+	if len(uris) < workers {
+		workers = len(uris)
+	}
+	tasks := make(chan string)
+	wg := utils.ErrWaitGroup{}
+
+	nbErrors := atomic.Int64{}
+	for range workers {
+		wg.Go(func() error {
+			for uri := range tasks {
+				if err := s.Delete(ctx, uri, options...); err != nil {
+					if nbErrors.Add(1) < maxErrors {
+						wg.AppendError(err)
+					}
+				}
+			}
+			return nil
+		})
+	}
+	for _, uri := range uris {
+		tasks <- uri
+	}
+	close(tasks)
+
+	errs := utils.MergeErrors(true, nil, wg.Wait()...)
+	if nbErrors.Load() >= maxErrors {
+		errs = utils.MergeErrors(true, errs, utils.MakeTemporary(fmt.Errorf("[...] total: %d errors", nbErrors.Load())))
+	}
+
+	return errs
+}
+
+func (s fileSystemStrategy) Exist(ctx context.Context, uri string) (bool, error) {
+	if _, err := os.Stat(uri); err != nil {
+		if os.IsNotExist(err) {
+			return false, geocubeStorage.ErrFileNotFound
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+func (s fileSystemStrategy) GetAttrs(ctx context.Context, uri string) (geocubeStorage.Attrs, error) {
+	f, err := os.Open(uri)
+	if err != nil {
+		return geocubeStorage.Attrs{}, fmt.Errorf("failed to open file: %w", formatError(err))
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		return geocubeStorage.Attrs{}, err
+	}
+
+	// Only the first 512 bytes are used to sniff the content type.
+	buffer := make([]byte, 512)
+	_, err = f.Seek(0, io.SeekStart)
+	if err != nil {
+		return geocubeStorage.Attrs{}, err
+	}
+
+	b, err := f.Read(buffer)
+	if err != nil && err != io.EOF {
+		return geocubeStorage.Attrs{}, err
+	}
+
+	buffer = buffer[:b]
+
+	// Always returns a valid content-type and "application/octet-stream"
+	// if no others seemed to match.
+	contentType := http.DetectContentType(buffer)
+	return geocubeStorage.Attrs{
+		ContentType:  contentType,
+		StorageClass: "filesystem",
+		Size:         fi.Size(),
+	}, nil
+}
+
+func (s fileSystemStrategy) StreamAt(key string, off int64, n int64) (io.ReadCloser, int64, error) {
+	f, err := os.Open(key)
+	if err != nil {
+		return nil, 0, fmt.Errorf("failed to open file: %w", formatError(err))
+	}
+	defer f.Close()
+
+	return io.NopCloser(f), 0, nil
+}
