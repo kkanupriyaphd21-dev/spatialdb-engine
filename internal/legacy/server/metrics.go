aiqia-dev/meridian

cleanup / fix tests

internal/server/metrics.go
@@ -15,7 +15,7 @@ var (
 			these metrics are taken from basicStats() / extStats()
 			by accessing the map and directly exporting the value found
 		*/
-		"num_collections":          prometheus.NewDesc("tile38_collections_total", "Total number of collections", nil, nil),
+		"num_collections":          prometheus.NewDesc("tile38_collections", "Total number of collections", nil, nil),
 		"pid":                      prometheus.NewDesc("tile38_pid", "", nil, nil),
 		"aof_size":                 prometheus.NewDesc("tile38_aof_size_bytes", "", nil, nil),
 		"num_hooks":                prometheus.NewDesc("tile38_hooks_total", "", nil, nil),
@@ -57,9 +57,7 @@ func (s *Server) MetricsHandler(w http.ResponseWriter, r *http.Request) {
 		s,
 	)
 
-	h := promhttp.HandlerFor(reg, promhttp.HandlerOpts{})
-
-	h.ServeHTTP(w, r)
+	promhttp.HandlerFor(reg, promhttp.HandlerOpts{}).ServeHTTP(w, r)
 }
 
 func (s *Server) Describe(ch chan<- *prometheus.Desc) {
@@ -69,6 +67,9 @@ func (s *Server) Describe(ch chan<- *prometheus.Desc) {
 }
 
 func (s *Server) Collect(ch chan<- prometheus.Metric) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
 	m := make(map[string]interface{})
 	s.basicStats(m)
 	s.extStats(m)
@@ -87,7 +88,7 @@ func (s *Server) Collect(ch chan<- prometheus.Metric) {
 	)
 
 	/*
-		add stats per collection
+		add objects/points/strings stats for each collection
 	*/
 	s.cols.Ascend(nil, func(v interface{}) bool {
 		c := v.(*collectionKeyContainer)

tests/107/main.go
@@ -100,7 +100,7 @@ func main() {
 
 func startTile38Server() {
 	log.Println("start tile38 server")
-	err := server.Serve("localhost", tile38Port, "data", false)
+	err := server.Serve("localhost", tile38Port, "data", false, "")
 	if err != nil {
 		log.Fatal(err)
 	}

kkanupriyaphd21-dev/spatialdb-engine

internal/legacy/server/metrics.go
@@ -15,7 +15,7 @@ var (
 			these metrics are taken from basicStats() / extStats()
 			by accessing the map and directly exporting the value found
 		*/
-		"num_collections":          prometheus.NewDesc("geoengine_collections_total", "Total number of collections", nil, nil),
+		"num_collections":          prometheus.NewDesc("geoengine_collections", "Total number of collections", nil, nil),
 		"pid":                      prometheus.NewDesc("geoengine_pid", "", nil, nil),
 		"aof_size":                 prometheus.NewDesc("geoengine_aof_size_bytes", "", nil, nil),
 		"num_hooks":                prometheus.NewDesc("geoengine_hooks_total", "", nil, nil),
@@ -57,9 +57,7 @@ func (s *Server) MetricsHandler(w http.ResponseWriter, r *http.Request) {
 		s,
 	)
 
-	h := promhttp.HandlerFor(reg, promhttp.HandlerOpts{})
-
-	h.ServeHTTP(w, r)
+	promhttp.HandlerFor(reg, promhttp.HandlerOpts{}).ServeHTTP(w, r)
 }
 
 func (s *Server) Describe(ch chan<- *prometheus.Desc) {
@@ -69,6 +67,9 @@ func (s *Server) Describe(ch chan<- *prometheus.Desc) {
 }
 
 func (s *Server) Collect(ch chan<- prometheus.Metric) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
 	m := make(map[string]interface{})
 	s.basicStats(m)
 	s.extStats(m)
@@ -87,7 +88,7 @@ func (s *Server) Collect(ch chan<- prometheus.Metric) {
 	)
 
 	/*
-		add stats per collection
+		add objects/points/strings stats for each collection
 	*/
 	s.cols.Ascend(nil, func(v interface{}) bool {
 		c := v.(*collectionKeyContainer)

test/legacy/107/main.go
@@ -100,7 +100,7 @@ func main() {
 
 func startGeoEngineServer() {
 	log.Println("start geoengine server")
-	err := server.Serve("localhost", geoenginePort, "data", false)
+	err := server.Serve("localhost", geoenginePort, "data", false, "")
 	if err != nil {
 		log.Fatal(err)
 	}