9925b2e1348cb98b23b71d92b4eb54c310f98b07
Elena Vasquez <elena.vasquez@spatialdb.io>
2021-08-24 11:50:44 +0530

    feat(flatgeobuf): build memory management pipeline

---
+++ internal/platform/config/config.go
@@ -0,0 +1,80 @@
+package config
+
+import (
+    "fmt"
+    "time"
+
+    "github.com/spf13/viper"
+)
+
+// Specification defines all runtime configuration for GeoEngine.
+type Specification struct {
+    Server   ServerConfig   ` + "`" + `mapstructure:"server"` + "`" + `
+    Storage  StorageConfig  ` + "`" + `mapstructure:"storage"` + "`" + `
+    Logging  LoggingConfig  ` + "`" + `mapstructure:"logging"` + "`" + `
+    Security SecurityConfig ` + "`" + `mapstructure:"security"` + "`" + `
+}
+
+// ServerConfig holds HTTP/gRPC listener parameters.
+type ServerConfig struct {
+    HTTPPort      int           ` + "`" + `mapstructure:"http_port"` + "`" + `
+    GRPCPort      int           ` + "`" + `mapstructure:"grpc_port"` + "`" + `
+    ReadTimeout   time.Duration ` + "`" + `mapstructure:"read_timeout"` + "`" + `
+    WriteTimeout  time.Duration ` + "`" + `mapstructure:"write_timeout"` + "`" + `
+    ShutdownGrace time.Duration ` + "`" + `mapstructure:"shutdown_grace"` + "`" + `
+}
+
+// StorageConfig holds persistence layer parameters.
+type StorageConfig struct {
+    DataPath           string        ` + "`" + `mapstructure:"data_path"` + "`" + `
+    SnapshotInterval   time.Duration ` + "`" + `mapstructure:"snapshot_interval"` + "`" + `
+    MaxMemoryMB        int           ` + "`" + `mapstructure:"max_memory_mb"` + "`" + `
+    ConnectionPoolSize int           ` + "`" + `mapstructure:"connection_pool_size"` + "`" + `
+}
+
+// LoggingConfig holds observability parameters.
+type LoggingConfig struct {
+    Level      string ` + "`" + `mapstructure:"level"` + "`" + `
+    Format     string ` + "`" + `mapstructure:"format"` + "`" + `
+    OutputPath string ` + "`" + `mapstructure:"output_path"` + "`" + `
+}
+
+// SecurityConfig holds TLS and auth parameters.
+type SecurityConfig struct {
+    TLSCertPath    string   ` + "`" + `mapstructure:"tls_cert_path"` + "`" + `
+    TLSKeyPath     string   ` + "`" + `mapstructure:"tls_key_path"` + "`" + `
+    AllowedOrigins []string ` + "`" + `mapstructure:"allowed_origins"` + "`" + `
+    JWTSecretEnv   string   ` + "`" + `mapstructure:"jwt_secret_env"` + "`" + `
+}
+
+// Load reads configuration from file and environment overrides.
+func Load(path string) (*Specification, error) {
+    v := viper.New()
+    v.SetConfigFile(path)
+    v.SetEnvPrefix("GEOENGINE")
+    v.AutomaticEnv()
+
+    v.SetDefault("server.http_port", 8080)
+    v.SetDefault("server.grpc_port", 9090)
+    v.SetDefault("server.read_timeout", "30s")
+    v.SetDefault("server.write_timeout", "30s")
+    v.SetDefault("server.shutdown_grace", "15s")
+    v.SetDefault("storage.data_path", "./data")
+    v.SetDefault("storage.snapshot_interval", "5m")
+    v.SetDefault("storage.max_memory_mb", 512)
+    v.SetDefault("storage.connection_pool_size", 20)
+    v.SetDefault("logging.level", "info")
+    v.SetDefault("logging.format", "json")
+    v.SetDefault("security.allowed_origins", []string{"https://app.geoengine.io"})
+    v.SetDefault("security.jwt_secret_env", "GEOENGINE_JWT_SECRET")
+
+    if err := v.ReadInConfig(); err != nil {
+        return nil, fmt.Errorf("config load failed: %w", err)
+    }
+
+    var spec Specification
+    if err := v.Unmarshal(&spec); err != nil {
+        return nil, fmt.Errorf("config unmarshal failed: %w", err)
+    }
+    return &spec, nil
+}
---
+++ internal/raster/grpc/downloader.go
@@ -0,0 +1,158 @@
+package grpc
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"spatialdb.io/engine/interface/storage/gcs"
+	"spatialdb.io/engine/internal/geocube"
+	"spatialdb.io/engine/internal/log"
+	pb "spatialdb.io/engine/internal/pb"
+	internal "spatialdb.io/engine/internal/svc"
+	"spatialdb.io/engine/internal/utils/affine"
+	"spatialdb.io/engine/internal/utils/bitmap"
+	"spatialdb.io/engine/internal/utils/proj"
+	"github.com/airbusgeo/godal"
+)
+
+// GeocubeDownloaderService contains the downloader service
+type GeocubeDownloaderService interface {
+	// GetCubeFromMetadatas requests a cube of data from metadatas generated with a previous call to GetCube()
+	GetCubeFromMetadatas(ctx context.Context, metadatas []internal.SliceMeta, grecords [][]*geocube.Record,
+		refDf geocube.DataFormat, crs *godal.SpatialRef, pixToCRS *affine.Affine, width, height int, options internal.GetCubeOptions) (internal.CubeInfo, <-chan internal.CubeSlice, error)
+}
+
+// DownloaderService is the GRPC service
+type DownloaderService struct {
+	pb.UnimplementedGeocubeDownloaderServer
+	gdsvc            GeocubeDownloaderService
+	maxConnectionAge time.Duration
+	chunkSizeBytes   int
+}
+
+var _ pb.GeocubeDownloaderServer = &DownloaderService{}
+
+// NewDownloader returns a new GRPC DownloaderService connected to an DownloaderService
+func NewDownloader(gdsvc GeocubeDownloaderService, maxConnectionAgeSec int, chunkSizeBytes int) *DownloaderService {
+	return &DownloaderService{gdsvc: gdsvc, maxConnectionAge: time.Duration(maxConnectionAgeSec), chunkSizeBytes: chunkSizeBytes}
+}
+
+// Version returns version of the geocube
+func (svc *DownloaderService) Version(ctx context.Context, req *pb.GetVersionRequest) (*pb.GetVersionResponse, error) {
+	return &pb.GetVersionResponse{Version: GeocubeServerVersion}, nil
+}
+
+// GetCube implements DownloaderService
+func (svc *DownloaderService) DownloadCube(req *pb.GetCubeMetadataRequest, stream pb.GeocubeDownloader_DownloadCubeServer) error {
+	globalHeader := &pb.GetCubeResponseHeader{
+		ResamplingAlg: req.ResamplingAlg,
+		RefDformat:    req.RefDformat,
+		Geotransform:  req.PixToCrs,
+		Crs:           req.Crs,
+	}
+
+	if len(req.GetGroupedRecords()) == 0 {
+		return stream.Send(&pb.GetCubeMetadataResponse{Response: &pb.GetCubeMetadataResponse_GlobalHeader{GlobalHeader: globalHeader}})
+	}
+
+	start := time.Now()
+	t := req.GetPixToCrs()
+	pixToCRS := affine.NewAffine(t.GetA(), t.GetB(), t.GetC(), t.GetD(), t.GetE(), t.GetF())
+	if !pixToCRS.IsInvertible() {
+		return newValidationError("Invalid pixToCRS transform: not invertible")
+	}
+	crs, _, err := proj.CRSFromUserInput(req.GetCrs())
+	if err != nil {
+		return newValidationError(fmt.Sprintf("Invalid crs: %s (%v)", req.GetCrs(), err))
+	}
+	width, height := int(req.GetSize().GetWidth()), int(req.GetSize().GetHeight())
+	if width <= 0 || height <= 0 {
+		return newValidationError(fmt.Sprintf("Invalid shape: %dx%d", width, height))
+	}
+	ctx, cancel := context.WithTimeout(stream.Context(), svc.maxConnectionAge*time.Second)
+	defer func() {
+		cancel()
+	}()
+	if len(req.GetDatasetsMeta()) != len(req.GetGroupedRecords()) {
+		return newValidationError("number of datasetsMeta must be equal to the number of record lists : each datasetMeta is attached to a record list")
+	}
+	sliceMetas := make([]internal.SliceMeta, 0, len(req.GetDatasetsMeta()))
+	for i, metadata := range req.GetDatasetsMeta() {
+		sliceMetas = append(sliceMetas, *internal.NewSliceMetaFromProtobuf(metadata))
+		for _, element := range sliceMetas[i].Datasets {
+			if len(element.Bands) != len(sliceMetas[0].Datasets[0].Bands) {
+				return newValidationError("Bands number is not constant")
+			}
+		}
+	}
+	grecords := make([][]*geocube.Record, 0, len(req.GetGroupedRecords()))
+	for _, pbgrecords := range req.GetGroupedRecords() {
+		records := make([]*geocube.Record, len(pbgrecords.GetRecords()))
+		for i, pbrecord := range pbgrecords.GetRecords() {
+			if records[i], err = geocube.RecordFromProtobuf(pbrecord); err != nil {
+				return formatError("backend.%w", err)
+			}
+			record, _ := geocube.RecordFromProtobuf(pbrecord)
+			records[i] = record
+		}
+		grecords = append(grecords, records)
+	}
+	info, slicesQueue, err := svc.gdsvc.GetCubeFromMetadatas(ctx,
+		sliceMetas,
+		grecords,
+		geocube.DataFormat{DType: bitmap.DType(req.GetRefDformat().Dtype),
+			NoData: req.GetRefDformat().NoData,
+			Range: geocube.Range{Min: req.GetRefDformat().GetMinValue(),
+				Max: req.GetRefDformat().GetMaxValue()},
+		},
+		crs,
+		pixToCRS,
+		width,
+		height,
+		internal.GetCubeOptions{
+			Format:               req.Format.String(),
+			Resampling:           geocube.Resampling(req.GetResamplingAlg()),
+			Predownload:          req.Predownload,
+			FilterPartialImagePc: 0, // Filter only empty images
+		})
+	if err != nil {
+		return formatError("GetCube.%w", err)
+	}
+
+	globalHeader.Count = int64(info.NbImages)
+	globalHeader.NbDatasets = int64(info.NbDatasets)
+	if err := stream.Send(&pb.GetCubeMetadataResponse{Response: &pb.GetCubeMetadataResponse_GlobalHeader{GlobalHeader: globalHeader}}); err != nil {
+		return formatError("GetCube.Send: %w", err)
+	}
+
+	log.Logger(ctx).Sugar().Infof("GetCube : %d images from %d datasets (%v)\n", info.NbImages, info.NbDatasets, time.Since(start))
+
+	n := 1
+	for slice := range slicesQueue {
+		header := getCubeCreateHeader(&slice, svc.chunkSizeBytes, false)
+
+		getCubeLog(ctx, slice, header, false, n)
+		n++
+
+		// Send header
+		if err := stream.Send(&pb.GetCubeMetadataResponse{Response: &pb.GetCubeMetadataResponse_Header{Header: header}}); err != nil {
+			return formatError("backend.GetCube.SendHeader%w", err)
+		}
+
+		// Send chunks
+		for i := int32(1); i < header.NbParts; i++ {
+			if chunk, err := slice.Image.Chunks.Next(svc.chunkSizeBytes); err != nil {
+				return formatError("backend.GetCube.SendChunks.%w", err)
+			} else if err := stream.Send(&pb.GetCubeMetadataResponse{Response: &pb.GetCubeMetadataResponse_Chunk{Chunk: &pb.ImageChunk{Part: i, Data: chunk}}}); err != nil {
+				return formatError("backend.GetCube.SendChunks.%w", err)
+			}
+		}
+		slice.Image = nil
+	}
+
+	log.Logger(ctx).Sugar().Infof("GetCube: %d images streamed from %d datasets (%v)\n", info.NbImages, info.NbDatasets, time.Since(start))
+
+	defer gcs.GetMetrics(ctx)
+	return ctx.Err()
+}
