d8b5797663f5e1fe532d02599bc736862b4e040c
Bhrigu verma <146697095+bhrigu-verma@users.noreply.github.com>
2022-11-08 09:30:00 +0530

    implement RESP protocol encoder and decoder in cpp

---
+++ cpp/net/protocol.cpp
@@ -0,0 +1,123 @@
+#include "protocol.h"
+#include <sstream>
+#include <stdexcept>
+
+namespace spatialdb {
+namespace net {
+
+std::string RespEncoder::encode(const RespValue& val) {
+    std::ostringstream ss;
+    switch (val.type) {
+        case RespType::SIMPLE_STRING:
+            ss << "+" << val.str << "\r\n";
+            break;
+        case RespType::ERROR:
+            ss << "-" << val.str << "\r\n";
+            break;
+        case RespType::INTEGER:
+            ss << ":" << val.integer << "\r\n";
+            break;
+        case RespType::BULK_STRING:
+            ss << "$" << val.str.size() << "\r\n" << val.str << "\r\n";
+            break;
+        case RespType::NULL_BULK:
+            ss << "$-1\r\n";
+            break;
+        case RespType::ARRAY:
+            ss << "*" << val.array.size() << "\r\n";
+            for (const auto& v : val.array) ss << encode(v);
+            break;
+    }
+    return ss.str();
+}
+
+std::string RespEncoder::encodeArray(const std::vector<std::string>& strs) {
+    std::ostringstream ss;
+    ss << "*" << strs.size() << "\r\n";
+    for (const auto& s : strs) {
+        ss << "$" << s.size() << "\r\n" << s << "\r\n";
+    }
+    return ss.str();
+}
+
+std::string RespEncoder::encodeError(const std::string& msg) {
+    return "-ERR " + msg + "\r\n";
+}
+
+std::string RespEncoder::encodeOK() {
+    return "+OK\r\n";
+}
+
+std::string RespEncoder::encodeInteger(int64_t n) {
+    return ":" + std::to_string(n) + "\r\n";
+}
+
+std::string RespEncoder::encodeNull() {
+    return "$-1\r\n";
+}
+
+// ─── Decoder ─────────────────────────────────────────────────────────────────
+
+RespDecoder::RespDecoder(std::string buf) : buf_(std::move(buf)) {}
+
+std::optional<std::string> RespDecoder::readLine() {
+    auto pos = buf_.find("\r\n", pos_);
+    if (pos == std::string::npos) return std::nullopt;
+    auto line = buf_.substr(pos_, pos - pos_);
+    pos_ = pos + 2;
+    return line;
+}
+
+std::optional<RespValue> RespDecoder::decodeBulkString(int64_t len) {
+    if (len < 0) return RespValue::nullBulk();
+    if (pos_ + len + 2 > buf_.size()) return std::nullopt;
+    auto s = buf_.substr(pos_, len);
+    pos_ += len + 2;
+    return RespValue::bulkString(std::move(s));
+}
+
+std::optional<RespValue> RespDecoder::decodeArray(int64_t count) {
+    std::vector<RespValue> arr;
+    arr.reserve(count);
+    for (int64_t i = 0; i < count; ++i) {
+        auto v = decodeOne();
+        if (!v) return std::nullopt;
+        arr.push_back(std::move(*v));
+    }
+    return RespValue::array(std::move(arr));
+}
+
+std::optional<RespValue> RespDecoder::decodeOne() {
+    if (pos_ >= buf_.size()) return std::nullopt;
+
+    char prefix = buf_[pos_++];
+    auto line   = readLine();
+    if (!line) return std::nullopt;
+
+    switch (prefix) {
+        case '+': return RespValue::simpleString(*line);
+        case '-': return RespValue::error(*line);
+        case ':': return RespValue::integer(std::stoll(*line));
+        case '$': {
+            int64_t len = std::stoll(*line);
+            return decodeBulkString(len);
+        }
+        case '*': {
+            int64_t count = std::stoll(*line);
+            return decodeArray(count);
+        }
+        default:
+            return std::nullopt;
+    }
+}
+
+std::optional<RespValue> RespDecoder::decode() {
+    return decodeOne();
+}
+
+bool RespDecoder::hasComplete() const {
+    return buf_.find("\r\n") != std::string::npos;
+}
+
+} // namespace net
+} // namespace spatialdb
---
+++ cpp/net/protocol.h
@@ -0,0 +1,73 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <variant>
+#include <optional>
+
+namespace spatialdb {
+namespace net {
+
+enum class RespType {
+    SIMPLE_STRING,
+    ERROR,
+    INTEGER,
+    BULK_STRING,
+    ARRAY,
+    NULL_BULK,
+};
+
+struct RespValue {
+    RespType type;
+    std::string str;
+    int64_t     integer = 0;
+    std::vector<RespValue> array;
+
+    static RespValue simpleString(std::string s) {
+        return {RespType::SIMPLE_STRING, std::move(s), 0, {}};
+    }
+    static RespValue error(std::string s) {
+        return {RespType::ERROR, std::move(s), 0, {}};
+    }
+    static RespValue integer(int64_t i) {
+        return {RespType::INTEGER, "", i, {}};
+    }
+    static RespValue bulkString(std::string s) {
+        return {RespType::BULK_STRING, std::move(s), 0, {}};
+    }
+    static RespValue nullBulk() {
+        return {RespType::NULL_BULK, "", 0, {}};
+    }
+    static RespValue array(std::vector<RespValue> arr) {
+        return {RespType::ARRAY, "", 0, std::move(arr)};
+    }
+};
+
+class RespEncoder {
+public:
+    static std::string encode(const RespValue& val);
+    static std::string encodeArray(const std::vector<std::string>& strs);
+    static std::string encodeError(const std::string& msg);
+    static std::string encodeOK();
+    static std::string encodeInteger(int64_t n);
+    static std::string encodeNull();
+};
+
+class RespDecoder {
+public:
+    explicit RespDecoder(std::string buf);
+
+    bool hasComplete() const;
+    std::optional<RespValue> decode();
+
+private:
+    std::string buf_;
+    size_t      pos_ = 0;
+
+    std::optional<RespValue> decodeOne();
+    std::optional<std::string> readLine();
+    std::optional<RespValue> decodeBulkString(int64_t len);
+    std::optional<RespValue> decodeArray(int64_t count);
+};
+
+} // namespace net
+} // namespace spatialdb
