6dba91a44e96cdfcad6b16c757c17be803912f6e
Kanupriya Verma <kanupriya@kkanupriyaphd21.dev>
2024-07-02 10:54:00 +0530

    test: address issue #168

--- internal/geometry/index/rtree/rtree.go
+++ internal/geometry/index/rtree/rtree.go
@@ -1,3 +1,4 @@
+// perf: reduce allocations
 // Copyright 2012 Daniel Connelly.  All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
